60edb7b90d35190bce9e6fe644b554f21ef61f79
Vincent Vo <orphanvin@gmail.com>
2025-03-07 08:26:52 -0800

    added pydantic validation to gam create group script

--- Projects/Workflow_Automation/Python/gsuite-gam-automation/gam_create_group_w_settings.py
+++ Projects/Workflow_Automation/Python/gsuite-gam-automation/gam_create_group_w_settings.py
@@ -2,7 +2,22 @@
 
 import subprocess
 import time
+from pydantic import EmailStr, field_validator, BaseModel, ValidationError
 
+
+class EmailInput(BaseModel):
+    email: EmailStr
+
+    # Custom validator to check iof the email ends with a specific domain
+    @field_validator('email')
+    def check_email_domain(cls, value):
+        # Define the allowed domain e.g. @example.com
+        allowed_domain = '@measurabl.com'
+
+        # Check if the email ends with the allowed domain
+        if not value.endswith(f'@measurabl.com'):
+            raise ValueError(f'Email must end with @{allowed_domain}')
+        return value
 
 def gam_create_group(gam_path, env, group_name, group_email, group_description):
     gam_command = [
@@ -51,8 +66,8 @@ def gam_apply_settings(gam_path, env, group_email, settings=None):
             "whoCanViewMembership": "ALL_MEMBERS_CAN_VIEW", # ALL_IN_DOMAIN_CAN_VIEW
             "whoCanContactOwner": "ALL_MEMBERS_CAN_CONTACT",
             "whoCanJoin": "INVITED_CAN_JOIN",
-            "archiveOnly": "true",
-            "whoCanPostMessage": "NONE_CAN_POST", # NONE_CAN_POST and archiveOnly needs to be True to do this, ALL_IN_DOMAIN_CAN_POST
+            "archiveOnly": "false",
+            "whoCanPostMessage": "ANYONE_CAN_POST", # NONE_CAN_POST and archiveOnly needs to be True to do this, ALL_IN_DOMAIN_CAN_POST
             "allowWebPosting": "false",
             "showInGroupDirectory": "false",
             "includeInGlobalAddressList": "false",
@@ -109,15 +124,16 @@ def main():
 
     # Loop through each email in the list
     for group_name in group_name:
-        
-        # Check if each email address contains '@'. If not, it's invalid.
-        if '@' not in group_name:
-            print("Error: Please entera a valid email address. Skipping...")
-            continue # Skip to the next email in the list
+        try:
+            group_name_input = EmailInput(email=group_name)
+            group_name = group_name_input.email
 
-    group_email = f"no-reply-{group_name.split('@')[0].replace('.', '-')}@{group_name.split('@')[1]}"
+            group_email = f"no-reply-{group_name.split('@')[0].replace('.', '-')}@{group_name.split('@')[1]}"
+            group_description = f"no-reply inbox for {group_name}"
 
-    group_description = f"no-reply inbox for {group_name}"
+        except ValidationError as e:
+            print(f'Invalid email {group_name} error {e}')
+            continue
 
     print(f'Creating group: {group_email}')
     gam_create_group(gam_path, env, group_name, group_email, group_description)
